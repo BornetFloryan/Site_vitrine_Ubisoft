e1dedddeaa10cd16c08d083ce26fe04262c19f1f
Floryan Bornet <93878409+BornetFloryan@users.noreply.github.com>
2023-10-15 01:41:35 +0200

    Add files via upload

---
+++ public/javaScript/innovation.js
@@ -0,0 +1,51 @@
+AOS.init();
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(event) {
+        event.preventDefault();
+
+        const targetId = this.getAttribute('href').substring(1);
+        const targetElement = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetElement.offsetTop,
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+var chevronContainer = document.getElementById("chevronContainer");
+
+var sectionCible = document.getElementById("down");
+
+chevronContainer.addEventListener("click", function() {
+
+    smoothScroll(sectionCible);
+});
+
+
+function smoothScroll(target) {
+    var targetPosition = target.offsetTop;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var duration = 570; 
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+        function easeInOutQuad(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
