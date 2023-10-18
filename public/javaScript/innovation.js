df9cdc259e92d2705ef90094012131ea5d8bb06e
ImmerAlex <105159979+ImmerAlex@users.noreply.github.com>
2023-10-18 14:21:41 +0200

    Add files via upload

--- public/javaScript/innovation.js
+++ public/javaScript/innovation.js
@@ -1,6 +1,6 @@
 AOS.init();
 
-var chevronContainer = document.getElementById("containerChev");
+var chevronContainer = document.querySelector(".chevron");
 
 var sectionCible = document.getElementById("down");
 
@@ -8,3 +8,29 @@ chevronContainer.addEventListener("click", function() {
 
     sectionCible.scrollIntoView();
 });
+
+
+function smoothScroll(target) {
+    var targetPosition = target.offsetTop;
+    var startPosition = window.pageYOffset;
+    var distance = targetPosition - startPosition;
+    var duration = 570; 
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var run = easeInOutQuad(timeElapsed, startPosition, distance, duration);
+        window.scrollTo(0, run);
+        if (timeElapsed < duration) requestAnimationFrame(animation);
+    }
+
+        function easeInOutQuad(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t + b;
+        t--;
+        return -c / 2 * (t * (t - 2) - 1) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
