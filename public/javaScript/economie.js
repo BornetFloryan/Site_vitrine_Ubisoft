174fac289f0fa5262a47e35db4957e599e5d54f2
ImmerAlex <105159979+ImmerAlex@users.noreply.github.com>
2023-10-19 09:06:00 +0200

    Add files via upload

--- public/javaScript/economie.js
+++ public/javaScript/economie.js
@@ -1,4 +1,5 @@
-const config = {
+const ctx1 = document.getElementById('myChart').getContext('2d');
+const myChart = new Chart(ctx1, {
     type: 'line',
     data: {
         labels: [1999, 2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021],
@@ -17,10 +18,31 @@ const config = {
             duration: 1000, // Durée de l'animation en millisecondes
         },
     },
-};
-
-// Créez un nouveau graphique en utilisant Chart.js
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, config);
+});
 
 
+
+
+const ctx2 = document.getElementById('myChart2').getContext('2d');
+
+const myChart2 = new Chart(ctx2, {
+    type: 'pie',
+    data: {
+        labels: ['Console en %', 'PC en %', 'Mobile en %', 'Autres en %'],
+        datasets: [{
+            data: [60, 9, 10, 26],
+            backgroundColor: ['#9b1e1f', '#F4A261', '#E9C46A', '#cfd2dc'],
+        }],
+    },
+    options: {
+        animation: {
+            animateRotate: true,
+            animateScale: true,
+        },
+        elements: {
+            arc: {
+                borderWidth: 2,
+            },
+        },
+    },
+});
